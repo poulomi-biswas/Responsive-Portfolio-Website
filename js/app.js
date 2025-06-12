8bb8de3dfe14fc99350d99294ee0454e610d9d27
poulomi-biswas <poulomibiswas110@gmail.com>
2025-06-12 16:12:43 +0530

    new changes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
-const header=document.querySelector("header");
+const header= document.querySelector("header");
+const ml_section= document.querySelector(".milestones");
+const ml_counters=document.querySelectorAll(".number span");
+const prt_section=document.querySelector(".portfolio");
+const zoom_icons=document.querySelectorAll(".zoom-icon");
+const modal_overlay=document.querySelector(".modal-overlay");
+const images =document.querySelectorAll(".images img");
+const prev_btn=document.querySelector(".prev-btn");
+const next_btn=document.querySelector(".next-btn");
 
 
 const first_skill=document.querySelector(".skill:first-child");
 const sk_counters=document.querySelectorAll(".counter span");
 const progress_bars=document.querySelectorAll(".skills svg circle");
 
+const links = document.querySelectorAll(".nav-link");
+const toggle_btn=document.querySelector(".toggle-btn");
+const hamburger=document.querySelector(".hamburger");
+
 window.addEventListener("scroll",()=>{
+    activeLink();
     if(!skillsPlayed)skillsCounter();
+    if(!mlPlayed) mlcounters();
 });
 
 /* --------------- Grab elements from DOM --------------- */
 
-/* --------------- Sticky Navbar --------------- */
+/* --------------- Sticky Navbar --- ------------ */
 function stickyNavbar(){
-    header.classList.toggle("scrolled", window.pageYOffset > 0);
+    header.classList.toggle("scrolled", window.scrollY > 0);
+
 }
-window.addEventListener("scroll", stickyNavbar);
+stickyNavbar();
+
+// window.addEventListener("scroll", stickyNavbar);
+window.addEventListener("scroll", () => {
+    stickyNavbar();
+    activeLink();
+  });
 
 /* --------------- Reveal Animation --------------- */
 let sr = ScrollReveal(
@@ -71,16 +92,44 @@ function skillsCounter(){
         );
 }
 
-/* --------------- Services Counter Animation --------------- */
+const swiper = new Swiper(".swiper",{
+    loop: true,
+    speed:500,
+    autoplay: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+},
+});
 
-/* --------------- Portfolio Filter Animation --------------- */
+function activeLink(){
+    let sections = document.querySelectorAll("section[id]");
+    let passedSections = Array.from(sections)
+    .map((sct, i) => {
+        return{
+            y: sct.getBoundingClientRect().top - header.offsetHeight,
+            id: i,
+        };
+    })
+    .filter((sct) => sct.y <= 0);
 
-/* --------------- Modal Pop Up Animation Animation --------------- */
 
-/* --------------- Modal Pop Up Animation Animation --------------- */
+    let currSectionID = passedSections.at(-1).id;
+    if (currSectionID !== undefined) {
+    links.forEach(l => l.classList.remove("active"));
+    links[currSectionID].classList.add("active"); 
+    }
 
-/* --------------- Change Active Link On Scroll --------------- */
+}
+//activeLink();
 
-/* --------------- Change Page Theme --------------- */
-
-/* --------------- Open & Close Navbar Menu --------------- */
+hamburger.addEventListener("click", () => {
+    document.body.classList.toggle("open");
+    document.body.classList.toggle("stopScrolling");
+});
+links.forEach((link) =>
+ link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+ })
+);
